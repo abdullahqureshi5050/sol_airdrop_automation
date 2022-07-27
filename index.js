d021a98fb74b1d82651a6988e43dbc3b410e39b0
Abdullah Qureshi <50483409+abdullahqureshi5050@users.noreply.github.com>
2022-07-27 20:07:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const {
 } = require('@solana/web3.js');
 
 (async ()=>{
-    const pubkey = new PublicKey('4b4raHcTK5u9FRFn9uggEZpYF6wg34hV4jgzprm8HURE');
+    const pubkey = new PublicKey('Enter Your Wallet Address Here');
     const connection = new Connection(clusterApiUrl('devnet'));
     for (let index = 0; index < 12; index++) {
         const tx = await connection.requestAirdrop( pubkey, 2*LAMPORTS_PER_SOL);
