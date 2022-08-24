ef4a7f260b70eafc9c56925d7f2ef4af5c01f195
Muhammad Usman Khan <usmankhan7419@gmail.com>
2022-08-24 17:45:29 +0500

    update to express and change public key as query parameter

--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 Solana airdrop automation. Get 24 SOL Free on Devnet.  
 
 # step 1: yarn install
-# step 2: Enter your wallet address in Publickey() constructor in index.js file.
-# step 3: node index.js
+# step 2: Enter your wallet address as http://localhost:3000/?publickey=public_key_of_your_wallet
+# step 3: Run the server
+for development mode
+you can write
+<code>npm run dev</code> or <code>yarn dev</code>
+
+and for production mode
+<code>npm run start</code> or <code>yarn start</code>
 
 Don't forget to follow and star this repo. 
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const {
-    Connection,
-    Keypair,
-    PublicKey,
-    clusterApiUrl,
-    LAMPORTS_PER_SOL
-} = require('@solana/web3.js');
+  Connection,
+  Keypair,
+  PublicKey,
+  clusterApiUrl,
+  LAMPORTS_PER_SOL,
+} = require("@solana/web3.js");
+const express = require("express");
+const app = express();
+app.get("/", async (req, res) => {
+  const publickey = req.query.publickey;
+  const pubKey = new PublicKey(publickey);
+  const connection = new Connection(clusterApiUrl("devnet"));
+  for (let index = 0; index < 12; index++) {
+    const tx = await connection.requestAirdrop(pubKey, 2 * LAMPORTS_PER_SOL);
+    await connection.confirmTransaction(tx);
+  }
+  res.status(200).json(`${lamports}`);
+});
 
-(async ()=>{
-    const pubkey = new PublicKey('Enter Your Wallet Address Here');
-    const connection = new Connection(clusterApiUrl('devnet'));
-    for (let index = 0; index < 12; index++) {
-        const tx = await connection.requestAirdrop( pubkey, 2*LAMPORTS_PER_SOL);
-        await connection.confirmTransaction(tx);    
-    } 
-})();
-
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
